766d80e0cca59d075a040e01b842bc5141a13f43
xshen053 <shenxiaxi26@gmail.com>
2023-05-15 23:17:32 -0700

    uploaded hw2

---
+++ HW2/app/src/main/java/hw2/ass2/Guitar37.java
@@ -0,0 +1,7 @@
+package hw2.ass2;// skeleton version of the class
+
+public class Guitar37 implements Guitar {
+   public static final String KEYBOARD =
+      "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";  // keyboard layout
+
+}
